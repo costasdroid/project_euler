6dadedd4b8d6558a95688555adfd625ed80016a4
Constantinos Lolas <costasdroid@gmail.com>
2015-01-06 10:35:55 +0200

    Create Group.java

---
+++ problem098/Group.java
@@ -0,0 +1,12 @@
+package problem098;
+
+import java.util.ArrayList;
+
+public class Group {
+	String repres;
+	ArrayList<String> words = new ArrayList<String>();
+
+	public Group(String s) {
+		repres = s;
+	}
+}

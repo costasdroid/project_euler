1fcc55bd441dace5068b8d46860c42e6c3468245
costasdroid <costasdroid@gmail.com>
2016-03-28 22:39:34 +0300

    solution113

---
+++ problem113/Solution113.java
@@ -0,0 +1,62 @@
+package problem113;
+
+import java.math.BigInteger;
+
+public class Solution113 {
+	
+/*
+	The idea is simple:
+	Create a "increasing" matrix 100x9 that each row represent the digits, each column the leading digit
+	ex.
+		1	2	3	4	5	6	7	8	9
+	1	1	1	1	1	1	1	1	1	1
+	2	9	8	7	6	5	4	3	2	1
+	3	45	36	48	21	15	10	6	3	1
+	...
+	
+	and a "decreasing" one
+	ex.
+		9	8	7	6	5	4	3	2	1
+	1	1	1	1	1	1	1	1	1	1
+	2	10	9	8	7	6	5	4	3	2
+	3	55	45	36	28	21	15	10	6	3
+	...
+	
+	and simply add each row to a total. Beware the 111, 222, 333, ..., 1111, 2222, ... (there are 9 in each row)
+*/
+	
+	public static void main(String[] args) {
+
+		long t = System.currentTimeMillis();
+		
+		BigInteger[][] inc = new BigInteger[2][9];
+		BigInteger[][] dec = new BigInteger[2][9];
+
+		for (int i = 0; i < 9; i++) {
+			inc[0][i] = BigInteger.ONE;
+			dec[0][i] = BigInteger.ONE;
+		}
+
+		BigInteger sum = BigInteger.valueOf(9);
+
+		for (int digits = 1; digits <= 99; digits++) {
+			inc[1][8] = inc[0][8];
+			dec[1][8] = dec[0][8].add(BigInteger.ONE);
+			for (int j = 7; j >= 0; j--) {
+				inc[1][j] = inc[1][j + 1].add(inc[0][j]);
+				dec[1][j] = dec[1][j + 1].add(dec[0][j]);
+			}
+			for (int j = 8; j >= 0; j--) {
+				sum = sum.add(inc[1][j]);
+				sum = sum.add(dec[1][j]);
+				inc[0][j] = inc[1][j];
+				dec[0][j] = dec[1][j];
+			}
+			sum = sum.subtract(BigInteger.valueOf(9));
+		}
+
+		System.out.println("Took: " + (System.currentTimeMillis() - t) + "ms");
+		System.out.println("Answer: " + sum + "");
+	}
+
+}

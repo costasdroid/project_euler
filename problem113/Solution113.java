ac31d0efc4013908cbec08a992e223b520e2b985
costasdroid <costasdroid@gmail.com>
2016-03-29 00:07:13 +0300

    Removed BigIntegers

--- problem113/Solution113.java
+++ problem113/Solution113.java
@@ -1,58 +1,45 @@
 package problem113;
 
-import java.math.BigInteger;
-
 public class Solution113 {
-	
-/*
-	The idea is simple:
-	Create a "increasing" matrix 100x9 that each row represent the digits, each column the leading digit
-	ex.
-		1	2	3	4	5	6	7	8	9
-	1	1	1	1	1	1	1	1	1	1
-	2	9	8	7	6	5	4	3	2	1
-	3	45	36	48	21	15	10	6	3	1
-	...
-	
-	and a "decreasing" one
-	ex.
-		9	8	7	6	5	4	3	2	1
-	1	1	1	1	1	1	1	1	1	1
-	2	10	9	8	7	6	5	4	3	2
-	3	55	45	36	28	21	15	10	6	3
-	...
-	
-	and simply add each row to a total. Beware the 111, 222, 333, ..., 1111, 2222, ... (there are 9 in each row)
-*/
-	
+
+	/*
+	 * The idea is simple: Create a "increasing" matrix 100x9 that each row
+	 * represent the digits, each column the leading digit ex. 1 2 3 4 5 6 7 8 9
+	 * 1 1 1 1 1 1 1 1 1 1 2 9 8 7 6 5 4 3 2 1 3 45 36 48 21 15 10 6 3 1 ...
+	 * 
+	 * and a "decreasing" one ex. 9 8 7 6 5 4 3 2 1 1 1 1 1 1 1 1 1 1 1 2 10 9 8
+	 * 7 6 5 4 3 2 3 55 45 36 28 21 15 10 6 3 ...
+	 * 
+	 * and simply add each row to a total. Beware the 111, 222, 333, ..., 1111,
+	 * 2222, ... (there are 9 in each row)
+	 */
+
 	public static void main(String[] args) {
 
 		long t = System.currentTimeMillis();
-		
-		BigInteger[][] inc = new BigInteger[2][9];
-		BigInteger[][] dec = new BigInteger[2][9];
+
+		long[] inc = new long[9];
+		long[] dec = new long[9];
 
 		for (int i = 0; i < 9; i++) {
-			inc[0][i] = BigInteger.ONE;
-			dec[0][i] = BigInteger.ONE;
+			inc[i] = 1;
+			dec[i] = 1;
 		}
 
-		BigInteger sum = BigInteger.valueOf(9);
+		long sum = 9;
 
 		for (int digits = 1; digits <= 99; digits++) {
-			inc[1][8] = inc[0][8];
-			dec[1][8] = dec[0][8].add(BigInteger.ONE);
+
+			sum += inc[8];
+			dec[8] = dec[8] + 1;
+			sum += dec[8];
 			for (int j = 7; j >= 0; j--) {
-				inc[1][j] = inc[1][j + 1].add(inc[0][j]);
-				dec[1][j] = dec[1][j + 1].add(dec[0][j]);
+				inc[j] = inc[j + 1] + inc[j];
+				sum += inc[j];
+				dec[j] = dec[j + 1] + dec[j];
+				sum += dec[j];
 			}
-			for (int j = 8; j >= 0; j--) {
-				sum = sum.add(inc[1][j]);
-				sum = sum.add(dec[1][j]);
-				inc[0][j] = inc[1][j];
-				dec[0][j] = dec[1][j];
-			}
-			sum = sum.subtract(BigInteger.valueOf(9));
+			sum -= 9;
 		}
 
 		System.out.println("Took: " + (System.currentTimeMillis() - t) + "ms");

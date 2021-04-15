f75a9a13a09d92c35263e23b4b588901311e9564
costasdroid <costasdroid@gmail.com>
2021-04-15 17:08:12 +0300

    prettier problem 129

--- problem129/Solution129.java
+++ problem129/Solution129.java
@@ -4,32 +4,43 @@ public class Solution129 {
     // For timing
     long t1 = System.currentTimeMillis();
 
+    // the question
     final long N = 1000000l;
 
+    // for help functions
     Numbers n = new Numbers();
 
-    long curN = N + 1;
+    // given than a^x=1 mod n it is certain that x<=φ(n)<=n
+    long number = N + (N % 2 == 0 ? 1 : 0);
+
     do {
-      curN = curN + 2;
-      if (curN % 5 == 0) curN = curN + 2;
-      long sol = curN * 9;
-      long totient = n.totient(sol);
-      if (totient < N) continue;
-      long tot = totient;
-      // System.out.println("totient of " + sol + " is " + tot);
+      // for every odd
+      number = number + 2;
+
+      // it must be GCD(n, 10)=1 mod 10 so it cannot be multiple of 5
+      if (number % 5 == 0) number = number + 2;
+
+      // R(k)=(10^k - 1)/9 so 10^k = 9k + 1
+      long n9 = number * 9;
+
+      // we want > N
+      if (n.totient(n9) <= N) continue;
+
+      // count k until 10^k = 1 mod 9n
+      long k = 0;
+
       long mod = 1l;
       do {
-        mod = (10 * mod) % sol;
-        // System.out.println("tot is " + (totient - tot + 1) + " New mod = " + mod);
-        tot--;
-      } while (tot > 0 && mod != 1);
-      // System.out.println(totient - tot);
-      if (totient - tot > N) {
+        mod = (10 * mod) % n9;
+        k++;
+      } while (mod != 1);
+
+      if (k > N) {
         break;
       }
     } while (true);
 
-    System.out.println("The solution is: " + curN);
+    System.out.println("The solution is: " + number);
     System.out.println("Took: " + (System.currentTimeMillis() - t1) + "ms");
   }
 
